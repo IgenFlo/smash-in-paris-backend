8acd7f4a7efaa5cb7ae7d09e5bdd36b42dec01a5
Flo-Galadrim <florian@galadrim.fr>
2024-08-02 19:11:34 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const port = process.env.PORT || 3000;
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+}
+bootstrap();
