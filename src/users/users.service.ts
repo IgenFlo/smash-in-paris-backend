40a0183e1c7103d67123f6529ea41494c22db7d2
IgenFlo <floehr31@gmail.com>
2024-08-03 00:16:49 +0200

    feat: users module created with create route

---
+++ src/users/users.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { prisma } from '../shared/client';
+
+@Injectable()
+export class UsersService {
+  create(createUserDto: CreateUserDto) {
+    const { discordId } = createUserDto;
+    return prisma.user.create({
+      data: { discordId },
+    });
+  }
+}
