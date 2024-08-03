c32a3ad8521e158ea0bb16e9ef92fcd369c2b825
IgenFlo <floehr31@gmail.com>
2024-08-03 19:42:03 +0200

    refactor: modules moved in app folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './app/users/users.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), UsersModule],
